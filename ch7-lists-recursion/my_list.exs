45d3b7e516db59f1a2232c712063a1e4002b430a
Ivan Manolov <ivan.manolov@reaktor.com>
2016-12-09 01:35:52 -0500

    Moar is moar

--- ch7-lists-recursion/my_list.exs
+++ ch7-lists-recursion/my_list.exs
@@ -2,11 +2,23 @@ defmodule MyList do
   def sum([]), do: 0
   def sum([head | tail]), do: head + sum(tail)
 
-  #ListAndRecursion-1
+  # ListAndRecursion-1
   def mapsum([], _func), do: 0
   def mapsum([head | tail], func), do: func.(head) + mapsum(tail, func)
 
-  #ListAndRecursion-2
-  def max([max_item]), do: max_item
-  def max([head | tail]), do: 
+  # ListAndRecursion-2
+  def max(list), do: _max(list, 0)
+  defp _max([], max_item), do: max_item
+  defp _max([head | tail], max_item) when head <= max_item, do: _max(tail, max_item)
+  defp _max([head | tail], max_item), do: _max(tail, head)
+
+  # ListsAndRecursion-3
+  def caesar([], _key), do: []
+  def caesar([head | tail], key) when head + key <= ?z do
+    [head + key | caesar(tail, key)]
+  end
+  def caesar([head | tail], key) do
+    [head + key - 26 | caesar(tail, key)]
+  end
+
 end
